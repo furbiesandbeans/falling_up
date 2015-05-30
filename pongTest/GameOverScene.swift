bbb4989a0ef324099548f3d3ca28e0062763c3ea
Erick Franco <erick.franco@verifi.com>
2015-05-30 08:38:16 -0700

    Initial commit

---
+++ pongTest/GameOverScene.swift
@@ -0,0 +1,113 @@
+//
+//  GameOverScene.swift
+//  fallingUp
+//
+//  Created by Erick Franco on 5/23/15.
+//  Copyright (c) 2015 Erick Franco. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    var playerScore: Int = 0
+    
+    init(size: CGSize, score:Int) {
+        super.init(size: size)
+        
+        var highScore: Int = 0
+        // load existing high scores or set up an empty array
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0] as! String
+        let path = documentsDirectory.stringByAppendingPathComponent("HighScore.plist")
+        let fileManager = NSFileManager.defaultManager()
+        
+        // check if file exists
+        if !fileManager.fileExistsAtPath(path) {
+            // create an empty file if it doesn't exist
+            if let bundle = NSBundle.mainBundle().pathForResource("DefaultFile", ofType: "plist") {
+                fileManager.copyItemAtPath(bundle, toPath: path, error:nil)
+            }
+        }
+        
+        if let rawData = NSData(contentsOfFile: path) {
+            // do we get serialized data back from the attempted path?
+            // if so, unarchive it into an AnyObject, and then convert to an array of HighScores, if possible
+            var scoreData: AnyObject? = NSKeyedUnarchiver.unarchiveObjectWithData(rawData);
+            highScore = scoreData as! Int;
+        }
+        
+        playerScore = score
+        backgroundColor = UIColor(red: 133/255, green: 208/255, blue: 240/255, alpha: 255/255)
+        
+        if playerScore > highScore {
+            highScore = playerScore
+            saveHighScore()
+            
+            let newHighLabel = SKLabelNode(fontNamed: "Chalkduster")
+            
+            newHighLabel.text = "NEW HIGHSCORE"
+            newHighLabel.fontColor = SKColor.blackColor()
+            newHighLabel.fontSize = 20
+            newHighLabel.position = CGPointMake(size.width/2, size.height - 100)
+            
+            let growAction = SKAction.scaleBy(2, duration: 1)
+            let shrinkAction = SKAction.scaleBy(0.5, duration: 1)
+            
+            let actionSequence = SKAction.sequence([growAction,shrinkAction])
+            
+            newHighLabel.runAction(SKAction.repeatActionForever(actionSequence))
+            addChild(newHighLabel)
+        }
+        
+        let label = SKLabelNode(fontNamed: "Chalkduster")
+        label.text = "Game Over!"
+        label.fontSize = 40
+        label.fontColor = SKColor.blackColor()
+        label.position = CGPoint(x: size.width/2, y: size.height/2 + 200)
+        addChild(label)
+        
+        let playerScoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        playerScoreLabel.text = "Your score: " + String(playerScore)
+        playerScoreLabel.fontSize = 20
+        playerScoreLabel.position = CGPoint(x: size.width/2, y: size.height/2 + 100.0)
+        addChild(playerScoreLabel)
+        
+        let highScoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        highScoreLabel.text = "High score: " + String(highScore)
+        highScoreLabel.fontSize = 20
+        highScoreLabel.position = CGPoint(x: size.width/2, y: size.height/2 + 0.0)
+        addChild(highScoreLabel)
+        
+        let promptLabel = SKLabelNode(fontNamed: "AppleSDGothicNeo-Bold")
+        promptLabel.text = "Touch anywhere to play again..."
+        promptLabel.fontSize = 25
+        promptLabel.position = CGPointMake(size.width/2, 200)
+        promptLabel.fontColor = SKColor.blackColor()
+        addChild(promptLabel)
+        
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+        let gameScene = GameScene(size: size)
+        self.view?.presentScene(gameScene, transition: reveal)
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func saveHighScore() {
+
+        let saveData = NSKeyedArchiver.archivedDataWithRootObject(playerScore);
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray;
+        let documentsDirectory = paths.objectAtIndex(0) as! NSString;
+        let path = documentsDirectory.stringByAppendingPathComponent("HighScore.plist");
+        
+        saveData.writeToFile(path, atomically: true);
+        
+    }
+}
--- pongTest/GameScene.swift
+++ pongTest/GameScene.swift
@@ -8,38 +8,177 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Ball   : UInt32 = 0b1       // 1
+    static let Screen : UInt32 = 0b10      // 2
+    static let Brick  : UInt32 = 0b100     // 4
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var ball: SKSpriteNode!
+    var screen: SKSpriteNode!
+    var player: SKSpriteNode!
+    var xVelocity: CGFloat = 200
+    var yVelocity: CGFloat = 200
+    var brickWallFrequency: CFTimeInterval = 0.8 // How often to create the wall
+    var lastTime: CFTimeInterval = 0.0
+    var score: Int = 0
+    var startTime: CFTimeInterval = 0.0
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        physicsWorld.contactDelegate = self
+        self.backgroundColor = UIColor(red: 133, green: 208, blue: 240, alpha: 255)
         
-        self.addChild(myLabel)
+        for var i: Int = 0; i < 2 ; i++ {
+            let bg = SKSpriteNode(imageNamed: "Background")
+            bg.name = "Bg"
+            bg.anchorPoint = CGPointZero
+            bg.position = CGPointMake(-200, CGFloat(i) * bg.size.height)
+            self.addChild(bg)
+        }
+        
+        screen = SKSpriteNode()
+        screen.position = CGPoint(x: 0, y: 0)
+        screen.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(origin: CGPoint(x:0,y:0), size: self.size))
+        screen.physicsBody?.categoryBitMask = PhysicsCategory.Screen
+        screen.physicsBody?.contactTestBitMask = PhysicsCategory.Ball
+        screen.physicsBody?.collisionBitMask = PhysicsCategory.All
+        
+        ball = SKSpriteNode(imageNamed: "Ball")
+        ball.position = CGPoint(x: self.size.width / 2, y: 50.0)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2.0)
+        ball.physicsBody?.dynamic = true
+        ball.physicsBody?.categoryBitMask = PhysicsCategory.Ball
+        ball.physicsBody?.contactTestBitMask = PhysicsCategory.Screen | PhysicsCategory.Brick
+        ball.physicsBody?.collisionBitMask = PhysicsCategory.All
+        
+        let spinningAction = SKAction.rotateByAngle(CGFloat(M_PI), duration: 2)
+        ball.runAction(SKAction.repeatActionForever(spinningAction))
+    
+        let scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: "
+        scoreLabel.fontSize = 20
+        scoreLabel.fontColor = UIColor.blackColor()
+        scoreLabel.position = CGPoint(x: size.width/2 - 100, y: size.height - 50.0)
+        
+        let scoreNumberLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreNumberLabel.name = "Score"
+        scoreNumberLabel.text = String(score)
+        scoreNumberLabel.fontSize = 20
+        scoreNumberLabel.fontColor = UIColor.blackColor()
+        scoreNumberLabel.position = CGPoint(x: size.width/2, y: size.height - 50.0)
+        
+        self.addChild(ball)
+        self.addChild(screen)
+        self.addChild(scoreLabel)
+        self.addChild(scoreNumberLabel)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch in (touches as! Set<UITouch>) {
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        for touch in (touches as! Set<UITouch>){
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let action = SKAction.moveToX(location.x, duration: 0)
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            ball.runAction(action)
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        if startTime == 0.0 { startTime = currentTime }
+        if lastTime == 0.0 { lastTime = currentTime }
+        
+        ball.physicsBody?.velocity = CGVector(dx:0.0,dy:yVelocity)
+        if lastTime + brickWallFrequency < currentTime {
+            createBrickWall()
+            lastTime = currentTime
+        }
+        
+        if brickWallFrequency > 0.35 {
+            brickWallFrequency = brickWallFrequency - 0.0005
+        }
+        
+        score = Int((currentTime - startTime) * 130)
+        
+        let scoreLabel = childNodeWithName("Score") as? SKLabelNode
+        scoreLabel?.text = String(score)
+        
+        self.enumerateChildNodesWithName("Bg", usingBlock: {node, stop in
+            if let bg = node as? SKSpriteNode {
+                bg.position = CGPointMake(bg.position.x, bg.position.y - 6)
+                
+                if bg.position.y <= -bg.size.height{
+                    bg.position = CGPointMake(bg.position.x, bg.position.y + bg.size.height * 2)
+                }
+            }
+            })
+        
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if ((firstBody.categoryBitMask & PhysicsCategory.Ball != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Screen != 0)) {
+                ballDidCollideWithWall(contact.contactNormal.dy)
+        }
+
+
+    }
+    
+    func ballDidCollideWithWall( dy: CGFloat){
+        if dy > 0.0 {
+            let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+            let gameOverScene = GameOverScene(size: self.size, score: score)
+            self.view?.presentScene(gameOverScene, transition: reveal)
+        }
+    }
+
+    
+    func createBrickWall(){
+       
+        let gapSize : CGFloat = ball.size.width+10
+        let gapLocation: CGFloat = CGFloat(random() % Int(self.size.width - gapSize))
+        let downAction = SKAction.moveByX(0.0, y: -(self.size.height+50), duration: 5.0)
+        
+        let wallTexture = SKTexture(imageNamed: "wallTexture")
+        
+        let leftWall = SKSpriteNode(texture: wallTexture , size: CGSizeMake(gapLocation, 25.0))
+        leftWall.anchorPoint = CGPointMake(0, 0)
+        leftWall.position = CGPointMake(0,self.size.height+20)
+        leftWall.physicsBody = SKPhysicsBody(rectangleOfSize: leftWall.size, center: CGPointMake(leftWall.size.width/2, leftWall.size.height/2))
+        leftWall.physicsBody?.categoryBitMask = PhysicsCategory.Brick
+        leftWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ball
+        leftWall.physicsBody?.collisionBitMask = PhysicsCategory.None
+        leftWall.runAction(SKAction.sequence([downAction, SKAction.removeFromParent()]))
+        self.addChild(leftWall)
+        
+        let rightWall = SKSpriteNode(texture: wallTexture, size: CGSizeMake(self.size.width - gapLocation - gapSize, 25.0))
+        rightWall.anchorPoint = CGPointMake(0, 0)
+        rightWall.position = CGPointMake(gapLocation+gapSize,self.size.height+20)
+        rightWall.physicsBody = SKPhysicsBody(rectangleOfSize: rightWall.size, center: CGPointMake(rightWall.size.width/2, rightWall.size.height/2))
+        rightWall.physicsBody?.categoryBitMask = PhysicsCategory.Brick
+        rightWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ball
+        rightWall.physicsBody?.collisionBitMask = PhysicsCategory.None
+        rightWall.runAction(SKAction.sequence([downAction, SKAction.removeFromParent()]))
+        self.addChild(rightWall)
+    
     }
 }
